08ac346d58c7caa73742989703d741acac575591
Harry Baldwin <harrythree@gmail.com>
2014-08-19 05:27:37 -0400

    switched referrer test for displayed referred users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @all_current_referrers = User.where(referrale_code: current_user.referrale_code)
+    @all_current_referrers = User.where(referrale_code: current_user.affiliate_code)
   end
 end
